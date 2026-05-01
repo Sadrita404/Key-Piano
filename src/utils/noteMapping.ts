b336a7a324715f266364cd0855a9c02fdb612d33
Sadrita Neogi <sadritaneogi@gmail.com>
2026-05-02 01:14:24 +0530

    Second row (ASDFGHJ) .- Next octave (do re mi fa so la ti do) Added

--- src/utils/noteMapping.ts
+++ src/utils/noteMapping.ts
@@ -10,3 +10,14 @@ export const noteMapping: { [key: string]: string } = {
   'y': 'A',
   'u': 'B',
   'i': 'C',
+
+   // Second row (ASDFGHJ) - Next octave (do re mi fa so la ti do)
+  'a': 'C',
+  's': 'D',
+  'd': 'E',
+  'f': 'F',
+  'g': 'G',
+  'h': 'A',
+  'j': 'B',
+  'k': 'C',
+  
