bbb26280cfcd6708ae8ce072c46be87b8516af61
Sadrita Neogi <sadritaneogi@gmail.com>
2026-05-02 01:07:34 +0530

    Constructoe variable added

--- src/utils/AudioEngine.ts
+++ src/utils/AudioEngine.ts
@@ -8,3 +8,20 @@ export class AudioEngine {
   private metronomeGainNode: GainNode;
   private masterGainNode: GainNode;
   private reverbNode: ConvolverNode | null = null;
+
+  constructor() {
+    this.audioContext = new (window.AudioContext || (window as any).webkitAudioContext)();
+    this.oscillators = new Map();
+    this.gainNodes = new Map();
+
+    // Create master gain node for overall volume control
+    this.masterGainNode = this.audioContext.createGain();
+    this.masterGainNode.connect(this.audioContext.destination);
+
+    // Create dedicated gain node for metronome volume control
+    this.metronomeGainNode = this.audioContext.createGain();
+    this.metronomeGainNode.gain.setValueAtTime(0.1, this.audioContext.currentTime);
+    this.metronomeGainNode.connect(this.masterGainNode);
+
+    this.initializeReverb();
+  }
